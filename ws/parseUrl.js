46dfaa756523e132974ef01c61ee960f0a57bfcc
igort <igor@gmail.com>
2026-05-19 19:06:19 +0300

    Commit::

--- ws/parseUrl.js
+++ ws/parseUrl.js
@@ -3,12 +3,13 @@ let {saveUp, getUp} = require("./saveUp");
 const {curl_direct} = require("./curl");
 
 async function parseUrl (json) {
+
     // let html = await getUp('curl.html')
     let cd = new Date().getTime()
     let html = await curl_direct(json.url, json)
     // console.log("qqqqq html ----->>>>>>>> ", json, {size: html?.length} );
     await saveUp('curl2.html', clearHtml(html))
-    return {html, cd: new Date().getTime() - cd};
+    return {html, ms: new Date().getTime() - cd};
 }
 
 module.exports = {parseUrl}
